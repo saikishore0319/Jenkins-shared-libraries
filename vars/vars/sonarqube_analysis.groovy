f9545e5d0076b20f8ccbf7cd1ca4d5033a044d82
SaiKishore <141355805+saikishore0319@users.noreply.github.com>
2025-09-12 20:14:48 +0530

    Delete vars/vars/sonarqube_analysis.groovy

--- vars/vars/sonarqube_analysis.groovy
+++
@@ -1,9 +0,0 @@
-def call(String SonarQubeApi, String ProjectName, String ProjectKey){
-  withSonarQubeEnv("${SonarQubeApi}") {
-    sh '''
-           $SONARQUBE_SERVER/bin/sonar-scanner \
-          -Dsonar.projectName=${ProjectName} \
-            -Dsonar.projectKey=${ProjectKey} \
-      '''
-      }
-}
