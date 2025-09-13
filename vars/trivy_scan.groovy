299e501799a404504968f5e2be77e11aa02cb18c
SaiKishore <141355805+saikishore0319@users.noreply.github.com>
2025-09-13 23:19:25 +0530

    Update trivy_scan.groovy

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,4 +1,4 @@
 def call(){
-  sh "trivy fs --exit-code 0 --severity HIGH,CRITICAL --format json -o trivy-report.json ."
+  sh "trivy fs . --exit-code 0 --severity HIGH,CRITICAL --format json -o trivy-report.json "
   archiveArtifacts artifacts: 'trivy-report.json', fingerprint: true
 }
