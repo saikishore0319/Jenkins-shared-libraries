13cf078400574023583749f5cba1e457d1cd881b
SaiKishore <141355805+saikishore0319@users.noreply.github.com>
2025-09-13 17:03:18 +0530

    Update update_compose_file.groovy

--- vars/update_compose_file.groovy
+++ vars/update_compose_file.groovy
@@ -1,6 +1,6 @@
 def call(String tag, String docker_user){
-  sh '''
-      sed -i 's| image: ${docker_user}/techify-backend:.*|image: image: ${docker_user}/techify-backend:${tag}'
-      sed -i 's| image: ${docker_user}/techify-frontend:.*|image: image: ${docker_user}/techify-frontend:${tag}'
-    '''
+  sh """
+      sed -i 's|image: ${docker_user}/techify-backend:.*|image: ${docker_user}/techify-backend:${tag}'  docker-compose.yml
+      sed -i 's|image: ${docker_user}/techify-frontend:.*|image: ${docker_user}/techify-frontend:${tag}'  docker-compose.yml
+    """
 }
