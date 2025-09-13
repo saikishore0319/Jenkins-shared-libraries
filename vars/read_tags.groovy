cb7cd758e6787c2174ca01668615980f564d7a81
SaiKishore <141355805+saikishore0319@users.noreply.github.com>
2025-09-13 19:29:35 +0530

    Update read_tags.groovy

--- vars/read_tags.groovy
+++ vars/read_tags.groovy
@@ -2,5 +2,5 @@ def call(){
   def versions = readYaml file: 'versions.yml'
   env.FRONTEND_TAG = versions.frontend.tag
   env.BACKEND_TAG = versions.backend.tag
-  echo 'Building frontend with "${FRONTEND_TAG}" and backend with "${BACKEND_TAG}"'
+  echo "Building frontend with ${FRONTEND_TAG} and backend with ${BACKEND_TAG}"
 }
